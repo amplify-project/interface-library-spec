c0c7e4bf7702f0fb402185c97f0ceee23965be17
Thomas Röggla <t.roggla@cwi.nl>
2025-10-30 14:58:58 +0100

    bash script takes output file param now

--- generate_pdf_from_spec.sh
+++ generate_pdf_from_spec.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-pandoc -V papersize:a4 -V geometry:margin=30mm -o amplify_interface_spec.pdf README.md
+if [ $# -ne 1 ]; then
+  echo "USAGE: $0 output_file"
+  exit 1
+fi
+
+output_file=$1
+
+pandoc -V papersize:a4 -V geometry:margin=30mm -o $output_file README.md
